3e1e6569949a82a89095e4dae1c998754046f580
Aidan Loughran <aidan@loughran.dev>
2024-01-17 02:09:10 +0000

    refactor(signals): remove `createDerived` callback args

--- packages/js/src/signal/function/create-derived.ts
+++ packages/js/src/signal/function/create-derived.ts
@@ -3,10 +3,6 @@ import type { ReadableSignal } from '../types';
 import { createSignal } from './create-signal';
 import { toReadOnlySignal } from './to-read-only-signal';
 
-type InferredArgs<T extends ReadableSignal<unknown>[]> = {
-  [K in keyof T]: ReturnType<T[K] extends ReadableSignal<infer U> ? ReadableSignal<U> : never>;
-};
-
 /**
  * Creates a derived signal, which is a special `ReadableSignal` where the value is automatically
  * derived from other signal(s).
@@ -16,19 +12,17 @@ type InferredArgs<T extends ReadableSignal<unknown>[]> = {
  *   you may even use another derived signal as a dependency, however exercise caution as
  *   introducing circular dependencies will freeze your program because there is not yet any
  *   mechnanism to detect them (TODO!)
- * @param callback A callback that accepts a mapped version of the values of the signals given in
- *   `dependencies` array. The returned value is the value is the new value of the derived signal.
- *   If your signal depends on multiple other signals, you will need to manually narrow the type.
- *   This is because the inferred value type becomes a union of all possible values (TODO!)
+ * @param callback A callback, called whenever the signal needs re-calculating, that returns the
+ *   calculated value of the derived signal.
  */
-export const createDerived = <TOutput, TInputs extends ReadableSignal<unknown>[]>(
-  dependencies: TInputs,
-  callback: (...args: InferredArgs<TInputs>) => TOutput,
-): ReadableSignal<TOutput> => {
-  const signal = createSignal(callback(...(dependencies.map((d) => d()) as InferredArgs<TInputs>)));
+export const createDerived = <T>(
+  dependencies: ReadableSignal<unknown>[],
+  callback: () => T,
+): ReadableSignal<T> => {
+  const signal = createSignal(callback());
 
   const compute = () => {
-    const newValue = callback(...(dependencies.map((d) => d()) as InferredArgs<TInputs>));
+    const newValue = callback();
     if (signal() !== newValue) {
       signal.set(newValue);
     }
