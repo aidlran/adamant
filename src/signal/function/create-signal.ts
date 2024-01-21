9b2c804eda2c62e24054ce9e6cb3cd39beae5840
Aidan Loughran <aidan@loughran.dev>
2024-01-21 05:16:18 +0000

    feat(signals): `createEffect` returns an unsubscribe function

--- src/signal/function/create-signal.ts
+++ src/signal/function/create-signal.ts
@@ -2,19 +2,32 @@ import { queueUniqueMicrotask } from '../../microtask/function/queue-unique-micr
 import type { Subscriber, WritableSignal } from '../types.js';
 
 let effectListener: (() => void) | undefined;
+let effectUnsubscribeSet: Set<() => void> | undefined;
 
 export function createEffect(callback: () => void) {
   effectListener = callback;
+  effectUnsubscribeSet = new Set();
   callback();
-  effectListener = undefined;
+  const unsubscribe = function (this: Set<() => void>) {
+    this.forEach((fn) => fn());
+  }.bind(effectUnsubscribeSet);
+  effectListener = effectUnsubscribeSet = undefined;
+  return unsubscribe;
 }
 
 export function createSignal<T>(initialValue: T): WritableSignal<T> {
   let currentValue = initialValue;
-  const subscribers = new Set<Subscriber<T>>();
+  const subscribers = new Set<Subscriber<T> | (() => void)>();
+
+  function unsubscribe(this: Subscriber<T> | (() => void)) {
+    subscribers.delete(this);
+  }
 
   function signal() {
-    effectListener && subscribers.add(effectListener);
+    if (effectListener) {
+      subscribers.add(effectListener);
+      effectUnsubscribeSet!.add(unsubscribe.bind(effectListener));
+    }
     return currentValue;
   }
 
@@ -22,17 +35,17 @@ export function createSignal<T>(initialValue: T): WritableSignal<T> {
     subscribers.forEach((subscriber) => subscriber(currentValue));
   }
 
-  signal.set = function (newValue: T) {
+  signal.set = (newValue: T) => {
     currentValue = newValue;
     // TODO: to make even more efficient: notify computed stores first,
     // have them calculate, then notify all subscribers in one pass
     queueUniqueMicrotask(push);
   };
 
-  signal.subscribe = function (subscriber: Subscriber<T>) {
+  signal.subscribe = (subscriber: Subscriber<T>) => {
     subscribers.add(subscriber);
     subscriber(currentValue);
-    return () => subscribers.delete(subscriber);
+    return unsubscribe.bind(subscriber);
   };
 
   return signal;
--- src/signal/types.ts
+++ src/signal/types.ts
@@ -2,6 +2,7 @@ export type Subscriber<T> = (newValue: T) => unknown;
 
 export interface ReadableSignal<T> {
   (): T;
+  /** @deprecated Use `createEffect` */
   subscribe: (subscriber: Subscriber<T>) => () => void;
 }
 
